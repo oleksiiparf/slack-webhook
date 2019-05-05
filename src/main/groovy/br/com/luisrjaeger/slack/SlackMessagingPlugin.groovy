6dfe0fcdd1cff8bfa6a4d899c587f41886578fbc
Luís Jaeger <904975@lojasrenner.com.br>
2019-05-05 12:17:43 -0300

    first commit

---
+++ src/main/groovy/br/com/luisrjaeger/slack/SlackMessagingPlugin.groovy
@@ -0,0 +1,23 @@
+package br.com.luisrjaeger.slack
+
+import br.com.luisrjaeger.slack.model.Message
+import br.com.luisrjaeger.slack.task.SlackTask
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class SlackMessagingPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        def messages = project.container(Message)
+        project.extensions.add("slack", messages)
+
+        messages.all { message ->
+            project.tasks.create("post${message.name.capitalize()}ToSlack", SlackTask) { task ->
+                messageToSend = message
+                description "Publish ${message.name} message to slack"
+            }
+        }
+    }
+
+}
